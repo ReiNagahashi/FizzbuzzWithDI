732119f4c63f98d51e5354e044443ace41cd5686
Hashi <rerenaga9170@icloud.com>
2026-01-08 15:31:53 +0900

    prepared DI given concrete instances

--- fizzbuzz_sequence_printer_good/fizzbuzz_sequence_printer.go
+++ fizzbuzz_sequence_printer_good/fizzbuzz_sequence_printer.go
@@ -1,12 +1,42 @@
 package fizzbuzzsequenceprintergood
 
+import (
+	"fmt"
+)
+
 // 😄詳細への結合を避け、固有のロジックとそのための設計だけを残した実装
+// インスタンス、インターフェースをメンバ変数として持たせるようにした
+// →この実装によって、単体テストでは、モックオブジェクトを使って
+// 「依存を意図した通りにコールしていれば良い」というスタンスで実装できる
+
+
+type NumberConverter interface{
+	Convert(n int)string
+}
+
+// outputInterfaceを準備した意図：クライアントコードの「printによる出力」という
+// テスト困難さを、「writeメソッドを呼ぶ」という行為に抽象化するため
+// これによりモックを使って「Writeメソッドが "Fizz" という引数で呼ばれたか？」
+// を確認するだけで済む!
+type OutputInterface interface{
+	write(data string )
+}
 
 // ①インターフェースをメンバ変数と持つことで、別パッケージの変更による影響を防ぐ
 type FizzBuzzSequencePrinter struct{
-	
+	fizzBuzz NumberConverter
+	output OutputInterface
 }
 
-func NewFizzBuzzSequencePrinter() *FizzBuzzSequencePrinter{
-	return &FizzBuzzSequencePrinter{}
+func NewFizzBuzzSequencePrinter(converter NumberConverter, output OutputInterface) *FizzBuzzSequencePrinter{
+	return &FizzBuzzSequencePrinter{fizzBuzz: converter, output: output}
+}
+
+func (p *FizzBuzzSequencePrinter) PrintRange(begin, end int) {
+	
+	for i := begin; i <= end; i++{
+		text := p.fizzBuzz.Convert(i)
+		formattedText := fmt.Sprintf("%d %s\n", i, text)
+		p.output.write(formattedText)
+	}
 }
---
+++ fizzbuzz_sequence_printer_good/fizzbuzz_sequence_printer_test.go
@@ -0,0 +1,48 @@
+package fizzbuzzsequenceprintergood
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/mock/gomock"
+)
+
+
+
+
+type MockOutputObject struct{
+	mock.Mock
+}
+
+func (o *MockOutputObject) write(data string){
+	o.Called(data)
+} 
+
+func TestPrintRange(t *testing.T){
+	tests := []struct {
+		name 		string
+		start, end 	int
+		expectCalls int
+	}{
+		{"Normal Case", 1, 5, 5},
+		{"No Op Case (Start > End)", 0, -1, 0},
+		{"Single Item", 1, 1, 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockConverter := NewMockNumberConverter(ctrl)
+			mockOutput := NewMockOutputInterface(ctrl)
+
+			// テーブル定義に基づいて期待値を設定
+			mockConverter.EXPECT().Convert(gomock.Any()).Times(tt.expectCalls)
+			mockOutput.EXPECT().write(gomock.Any()).Times(tt.expectCalls)
+
+			p := NewFizzBuzzSequencePrinter(mockConverter, mockOutput)
+			p.PrintRange(tt.start, tt.end)
+		})
+	}
+}
